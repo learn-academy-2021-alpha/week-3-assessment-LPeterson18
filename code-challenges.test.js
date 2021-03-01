491ce545179ed3148f508f7be23b7c42c9998edc
LPeterson18 <lpeterson@chorus.net>
2021-02-28 17:28:33 -0800

    Finished all parts

--- code-challenges.test.js
+++ code-challenges.test.js
@@ -24,14 +24,65 @@
 // Example input: 10
 // Expected output: [1, 1, 2, 3, 5, 8, 13, 21, 34, 55]
 
-
-
-
+describe("When createFibArray is called", () => {
+  it("return [1, 1, 2, 3, 5,8] if 6 is called",() => {
+    //arrange
+    const argument = 6
+    //act
+    const actualResult = createFibArray(argument)
+    //assert
+  expect(actualResult).toEqual([1, 1, 2, 3, 5, 8])
+  })
+  it("return [1, 1, 2, 3, 5, 8, 13, 21, 34, 55] if 10 is called",() => {
+    //arrange
+    const argument = 10
+    //act
+    const actualResult = createFibArray(argument)
+    //assert
+  expect(actualResult).toEqual([1, 1, 2, 3, 5, 8, 13, 21, 34, 55])
+  })
+})
 
 // b) Create the function that makes the test pass.
 
+//pseudocode
+//create a function that takes a number as an argument
+//use Array(number) to create array with "number" indices
+//fibonacci is every number is the sum of the two numbers before it
+//.map of array
+//each iteration add number using fibonacci to determine which number
+//first two numbers set to 1
+//afterward sum previous two array elements together to create new element
 
+// const createFibArray = (number) => {
+//   var fibArray = Array(number).fill(0)
+//   for(let i = 0; i < fibArray.length; i++){
+//     if(i === 0){
+//       fibArray[i] = 1
+//     } else if(i === 1){
+//       fibArray[i] = 1
+//     } else{
+//       fibArray[i] = fibArray[i - 2] + fibArray[i - 1]
+//     }
+//   }
+//   return fibArray
+// }
+// console.log(createFibArray(6));
+//works with for loop
+//GET TO WORK WITH .MAP
 
+const createFibArray = (number) => {
+  var fibArray = Array(number).fill(0)
+  return fibArray.map((number, i) => {
+    if(i === 0){
+      return fibArray[i] = 1
+    } else if(i === 1){
+      return fibArray[i] = 1
+    } else{
+      return fibArray[i] = fibArray[i - 2] + fibArray[i - 1]
+    }
+  })
+}
 
 
 // --------------------2) Create a function that takes in an array and returns a new array of only odd numbers sorted from least to greatest.
@@ -44,13 +95,40 @@ var fullArr1 = [4, 9, 0, "7", 8, true, "hey", 7, 199, -9, false, "hola"]
 var fullArr2 = ["hello", 7, 23, -823, false, 78, null, "67", 6, "number"]
 // Expected output: [-823, 7, 23]
 
-
+describe("When oddNumSorter is called", () => {
+  it("return [-9, 7, 9, 199] if [4, 9, 0, '7', 8, true, 'hey', 7, 199, -9, false, 'hola'] is called",() => {
+    //arrange
+    const argument = fullArr1
+    //act
+    const actualResult = oddNumSorter(argument)
+    //assert
+  expect(actualResult).toEqual([-9, 7, 9, 199])
+  })
+  it("return [-823, 7, 23] if ['hello', 7, 23, -823, false, 78, null, '67', 6, 'number'] is called",() => {
+    //arrange
+    const argument = fullArr2
+    //act
+    const actualResult = oddNumSorter(argument)
+    //assert
+  expect(actualResult).toEqual([-823, 7, 23])
+  })
+})
 
 
 // b) Create the function that makes the test pass.
 
+//pseudocode
+//create function
+//takes array as argument
+// use .filter to remove all non-number elements and all even numbers
+//rearrange array using .sort
+//return new Array
 
-
+const oddNumSorter = (array) => {
+  return array.filter(element => {
+    return typeof element === 'number' && element % 2 !== 0
+  }).sort((a,b) => a-b)
+}
 
 
 // --------------------3) Create a function that takes in a string of a single word and returns the middle letter of the word. If the word is an even number of letters, return the two middle letters.
@@ -63,14 +141,44 @@ var middleLetters1 = "alpha"
 var middleLetters2 = "rhinoceros"
 // Expected output: “oc”
 
-
-
-
+describe("When findStringMidLetter is called", () => {
+  it("return 'p' if 'alpha' is called",() => {
+    //arrange
+    const argument = middleLetters1
+    //act
+    const actualResult = findStringMidLetter(argument)
+    //assert
+  expect(actualResult).toEqual("p")
+  })
+  it("return 'oc' if 'rhinoceros' is called",() => {
+    //arrange
+    const argument = middleLetters2
+    //act
+    const actualResult = findStringMidLetter(argument)
+    //assert
+  expect(actualResult).toEqual("oc")
+  })
+})
 
 // b) Create the function that makes the test pass.
 
+//pseudocode
+//create function
+//takes in string as argument
+//if statement for even and if for odd to know whether to take 1 of 2 letters
+//.slice to remove letter or set of letters
+// 5 letter word need letter at index of 2 => .length of 5 letter word is 5 => subtract 1 = 4 => divide by 2 = 2 => remove 1 letter at 2 index
+// 6 letter word need letters at indices of 2 and 3 => .length of 6 letter word is 6 => divide by 2 = 3 => subtract 1 to get index 2 => remove 2 letters
+// indices used in .slice are from .length
+//return letters sliced out
 
-
+const findStringMidLetter = (string) => {
+  if(string.length % 2 === 0){
+    return string.slice((string.length / 2) - 1, (string.length/2) + 1)
+  } else if(string.length % 2 !== 0){
+    return string.slice(((string.length - 1) / 2), ((string.length-1)/2) + 1)
+  }
+}
 
 
 // --------------------4) Create a function that takes in an array and returns an array of the accumulating sum. An empty array should return an empty array.
@@ -78,7 +186,7 @@ var middleLetters2 = "rhinoceros"
 // a) Create a test with expect statements for each of the variables provided.
 
 var numbersToAdd1 = [2, 4, 45, 9]
-// Excpected output: [2, 6, 51, 60]
+// Expected output: [2, 6, 51, 60]
 
 var numbersToAdd2 = [0, 7, -8, 12]
 // Expected output: [0, 7, -1, 11]
@@ -86,8 +194,54 @@ var numbersToAdd2 = [0, 7, -8, 12]
 var numbersToAdd3 = []
 // Expected output: []
 
-
-
-
+describe("When createAccumSumArray is called", () => {
+  it("return [2, 6, 51, 60] if [2, 4, 45, 9] is called",() => {
+    //arrange
+    const argument = numbersToAdd1
+    //act
+    const actualResult = createAccumSumArray(argument)
+    //assert
+  expect(actualResult).toEqual([ 2, 6, 51, 60 ])
+  })
+  it("return [0, 7, -1, 11] if [0, 7, -8, 12] is called",() => {
+    //arrange
+    const argument = numbersToAdd2
+    //act
+    const actualResult = createAccumSumArray(argument)
+    //assert
+  expect(actualResult).toEqual([ 0, 7, -1, 11 ])
+  })
+  it("return [] if [] is called",() => {
+    //arrange
+    const argument = numbersToAdd3
+    //act
+    const actualResult = createAccumSumArray(argument)
+    //assert
+  expect(actualResult).toEqual([])
+  })
+})
 
 // b) Create the function that makes the test pass.
+
+//pseudocode
+//create a new function
+//takes an array as an argument
+//.map the array
+//set element equal to each element before plus current elements
+//return new Array
+
+const createAccumSumArray = (array) => {
+  var accumSumArray = array.map((number, index) => {
+    if(index === 0) {
+      console.log(array[index]);
+      return array[index] = array[index]
+    } else{
+      console.log(array[index]);
+      array[index] = array[index - 1] + array[index]
+      return array[index]
+    }
+  })
+  return accumSumArray
+}
+
+//
--- interview-questions.md
+++ interview-questions.md
@@ -10,87 +10,104 @@ Then, research the question to expand on your answer. Even if you feel you have 
 1. What is object destructuring?
 
   Your answer:
+  Object destructuring is using the key to call a value from an object and set it equal to a variable so that the entire object calling process does not need to occur every time the value needs to be called.  Especially useful if a deep nested value within an object is going to be called a number of times.
 
   Researched answer:
-
+  Object destructuring unpacks a value from an object and assigns it to a variable.
 
 
 2. What are React lifecycle methods? Provide three examples.
 
   Your answer:
+  React lifecycle methods are methods that occur during the different lifestyle phases of react applications.  construction(), render(), and setState() are all lifecycle methods.
+
 
   Researched answer:
-
+  React lifecycle methods are the methods that get called during the three lifecycle phases of components.  The lifecycle phases are mounting, updating , and unmounting.  Three methods include construction(), render(), and componentDidMount().
 
 
 3. What is the difference between a class and an object?
 
   Your answer:
+  An object is a specific instance of a class. Classes serve as the blueprint to create predefined objects. Classes have no values within them.
 
   Researched answer:
-
+  Classes are a template for creating objects.  They can be assigned values and methods that are given to objects created using the class.
 
 
 4. What is the difference between a HTML div and a span?
 
   Your answer:
+  div is a container that displays content on a webpage.  A span is similar, but contains different predefines stylistic features.
 
   Researched answer:
-
+  Span is used for small amounts of in-line code, while div is used for larger amounts of block-line code.
 
 
 5. What is a React Fragment (or <>) and why would you want to use it?
 
   Your answer:
+  <> defines the area of a react section.  Necessary if you want to display multiple sections in a react app.
 
   Researched answer:
-
+  React fragments are not rendered to the DOM so they allow you to group a list of children without creating extra nodes in the DOM. Faster and less memory usage than creating a wrapper div since it does not create a new node in the DOM.
 
 
 6. What does it mean to create a responsive UI? What are three options for creating responsive design?
 
   Your answer:
+  Responsive UI changes what the user sees as the user interacts with the webpage without the user having to refresh the webpage.  React, and any other javascript library that uses a virtual dom would be good options for creating responsive design.  PHP would also work.
 
   Researched answer:
-
+  Responsive UI is when the user interface of a website adjusts to fit different screen sizes and user experiences.  The three core principles are a fluid grid system, fluid image use, and media queries.
 
 
 7. What are props in React?
 
   Your answer:
+  Props allow variables defined in the main App.js file to be called within external components.  They allow for the flow of data back and forth between the App.js file and the components.
 
   Researched answer:
-
+  Props are arguments passed into components.  Props also allow you to pass data from one component to another.
 
 
 8. What is `this` in JavaScript?
 
   Your answer:
+  `this` is necessary when calling variables and methods defined within the same react app they are created.  It tells the decoder where to look for the variables and methods called.
 
   Researched answer:
-
+  Used to call global objects.
 
 
 9. STRETCH: What is a ternary operator in JavaScript?
 
   Your answer:
+  A ternary operator in JavaScript is a shortcut to replace an if/else statement. It checks if something is true or false and then does one thing or another depending on the response. It is structured like this: Thing that is checked if true or false ?  if true do _ : if false do _ .
 
   Researched answer:
-
+  Also known as a conditional operator , it takes in three arguments.  A condition, an expression to execute if the condition is truthy, and an expression to run if the condition is falsy.
 
 
 10. STRETCH: What is an anonymous function in JavaScript?
 
   Your answer:
+  An Anonymous function is a function without a name. They exist in specific built-in functions and can be called using `function`.
 
   Researched answer:
-
+  Functions stored in variables do not need names.  These are called anonymous functions.
 
 
 ## Looking Ahead: Terms for Next Week
 - Ruby
+  Ruby is an object oriented programming language.  It is dynamic and open-source.  It's a server side programming language, and has a simple style that makes it easy to read and write.
 - Object oriented programming
+  Object oriented programming was created to solve the problem of large complex code bases filled with dependencies that are difficult to edit without creating errors.  It makes it easier to create programs that consist of a connection of a number of small parts, as opposed to one large unmanageable piece of code.
 - RSpec
+  Rspec is a testing tool for Ruby.  It is designed with Behavior Driven Development in mind, which like Test Driven Development, has the tests written before the program.
 - Instance variable
+  An instance variable is a variable defined in a class, but outside of constructors, methods, or blocks.  It is created when an object is instantiated, and is usable by all constructors, methods, and blocks.  It is specific to each instance of the class, hence the name.
 - Ruby blocks
+  Ruby blocks are anonymous functions that can be passed into methods.  Enclosed in a do-end statement or in {}.  Arguments in blocks are defined using ||.
 - Ruby hashes
+  A ruby hash is like an array except instead of indices each value is indexed by an arbitrary value similar to objects.  It is a collection of key-value pairs.
